0d225706a3fab491588083f2f5ee4f08f7059ac5
jiya-talreja <talrejajiya115@gmail.com>
2026-05-02 17:23:41 +0530

    Enhance OCR processing with question extraction
    Refactor OCR functions to include question extraction.

--- backend/services/img_ext.py
+++ backend/services/img_ext.py
@@ -17,15 +17,16 @@ def run_ocr(pil_image):
     text = pytesseract.image_to_data(thresh, config="--oem 3 --psm 4",output_type=pytesseract.Output.DICT)
     fl=ocr_to_lines(text)
     print(fl)
-    return fl
+    questions=extract_questions(fl)
+    return questions
 def ocr_to_lines(data,con_thres=40):
     line_dict={}
     for i,word in enumerate(data['text']):
         word=word.strip()
         if word=="":
             continue
-        w=word.split()
-        if len(w)<=1 and w is not w.isdigit():
+        w=word.strip()
+        if len(w)<=1 and not w.isdigit():
             continue
         try:
             if(int(data['conf'][i])<=con_thres):
@@ -35,9 +36,65 @@ def ocr_to_lines(data,con_thres=40):
         block=data['block_num'][i]
         line=data['line_num'][i]
         key=(block,line)
-        if key not in line_dict:
+        if key not in line_dict:#if line dict empty
             line_dict[key]=[]
         line_dict[key].append(word)
-        print("LINE DICT : ",line_dict)
+        
     final_list=[]
-    
+    for k in sorted(line_dict.keys()):
+        sentence=" ".join(line_dict[k])
+        final_list.append(sentence)
+    return final_list
+def extract_questions(lines):#list of lines in paper
+    use_words=["how","when","why","which","where","who","whom",
+        "what","define","explain","difference",
+        "distinguish","short notes"
+    ]
+    ignore_words=[
+        "note","notes","instruction","general instructions",
+        "compulsory","permitted","max time",
+        "attempt","question paper","section",
+        "sections","marks","mark","attempted","paper"]
+    current_q=""
+    questions=[]
+    in_main=False
+    for line in lines:
+        line=line.lower().strip()
+        if not line:
+            continue
+        if any(word in line for word in ignore_words):
+            label="noise"
+        elif(
+            re.match(r'^\(?\d+[\).\s]', line) or#1. 1)
+            re.match(r'^q[\.\s-]*\d+', line)#Q..
+        ):
+            label="main"
+        elif(
+            re.match(r'^[a-z]\)', line) or#a) a.
+            re.match(r'^\([ivx]+\)', line) or#roman
+            re.match(r'^\d+\.\d+', line)#1.1
+        ):
+            label="subq"
+        elif(
+            "?" in line or
+            any(word in line for word in use_words)
+        ):
+            label="con"
+        else:
+            label="con"
+        if label=="noise":
+            continue
+        elif label=="main":
+            if current_q:
+                questions.append(current_q.strip())
+            current_q=line
+            in_main=True
+        elif label=="subq":
+            if in_main:
+                current_q+=" "+line
+        elif label=="con":
+                if current_q:
+                    current_q+=" "+line
+    if current_q:
+            questions.append(current_q.strip())
+    return questions
