7d9793fb3bcaf883d129076b9060a867e32cfabc
jiya-talreja <talrejajiya115@gmail.com>
2026-06-30 00:27:40 +0530

    Update input_handle.py

--- backend/services/input_handle.py
+++ backend/services/input_handle.py
@@ -1,7 +1,5 @@
 from pdf2image import convert_from_bytes
 from services.img_ext import run_ocr
-
-
 from docx import Document
 import io
 from PIL import Image
@@ -9,35 +7,40 @@ import fitz
 POPPLER_PATH = r"C:\Users\DELL\Downloads\Release-25.12.0-0\poppler-25.12.0\Library\bin"
 def pdf(file_bytes):
     doc = fitz.open(stream=file_bytes, filetype="pdf")
-    text_data = []
-    tesxt=""
-    fil=[]
+    text = ""
     for page in doc:
-        text = page.get_text().strip()
-        if text:
-            tesxt += text + "\n"
-    if tesxt.strip():
+        page_text = page.get_text().strip()
+        if page_text:
+            text += page_text + "\n"
+    if text.strip():
         print("TEXT PDF")
-        return text_data
+        return text
     print("SCANNED PDF")
-    images = convert_from_bytes(file_bytes, dpi=300, poppler_path=POPPLER_PATH)
+    images = convert_from_bytes(
+        file_bytes,
+        dpi=300,
+        poppler_path=POPPLER_PATH
+    )
+    ocr_text = ""
     for img in images:
-        text_data.append(run_ocr(img))
-    return text_data
+        page_text = run_ocr(img)      # run_ocr should also return a STRING
+        ocr_text += page_text + "\n"
+    return ocr_text
 def docs(file_bytes):
-    docx = Document(io.BytesIO(file_bytes))
-    text_data = ""
-    for para in docx.paragraphs:
-        text_data += para.text + "\n"
-    for table in docx.tables:
+    doc = Document(io.BytesIO(file_bytes))
+    text = ""
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+    for table in doc.tables:
         for row in table.rows:
             for cell in row.cells:
-                text_data += cell.text + "\n"
-    return text_data
+                text += cell.text + "\n"
+    return text
 def img_in(img_bytes_list):
-    text_data = ""
+    text = ""
     for img_b in img_bytes_list:
         img = Image.open(io.BytesIO(img_b))
-        #text_data += run_ocr(img) + "\n"
-        #print("TEXTTTTT : ",text_data)
-    return run_ocr(img)
+        text += run_ocr(img)
+        text += "\n"
+    return text
+  
