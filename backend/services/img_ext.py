ef562b12596612ff1dbe312241faa6b5e6ee16a7
jiya-talreja <talrejajiya115@gmail.com>
2026-06-30 00:19:33 +0530

    Refactor OCR functions for better text extraction
    Updated the OCR functions to improve text extraction and added functionality to return final text.

--- backend/services/img_ext.py
+++ backend/services/img_ext.py
@@ -17,8 +17,8 @@ def run_ocr(pil_image):
     text = pytesseract.image_to_data(thresh, config="--oem 3 --psm 4",output_type=pytesseract.Output.DICT)
     fl=ocr_to_lines(text)
     print(fl)
-    questions=extract_questions(fl)
-    return questions
+    final_text=" ".join(fl)
+    return final_text
 def ocr_to_lines(data,con_thres=40):
     line_dict={}
     for i,word in enumerate(data['text']):
@@ -39,55 +39,8 @@ def ocr_to_lines(data,con_thres=40):
         if key not in line_dict:#if line dict empty
             line_dict[key]=[]
         line_dict[key].append(word)
-        
     final_list=[]
     for k in sorted(line_dict.keys()):
         sentence=" ".join(line_dict[k])
         final_list.append(sentence)
     return final_list
-fixes = {
-    "11.": "ii.",
-    "Il.": "ii.",
-    "il.": "iii.",
-    "Ill.": "iii.",
-    "vil.": "vii.",
-    "Vill.": "viii.",
-    "vill.": "viii.",
-    "1s": "is"
-}
-def normalize(text):
-    for wrong, correct in fixes.items():
-        text = text.replace(wrong, correct)
-    return text
-def extract_questions(lines):
-    chunks = []
-    current = None
-    for line in lines:
-        line = normalize(line).strip()
-        if not line:
-            continue
-        if re.match(r'^(Q\.?\s*)?\d+\.', line, re.I) or re.match(r'^Q\.?\s*\d+', line, re.I):
-            if current:
-                content = current["content"]
-                roman = list(re.finditer(r'(?<=\s)(i|ii|iii|iv|v|vi|vii|viii|ix|x)\.', content, re.I))
-                if roman and "multiple choice" not in content.lower():
-                    for i, m in enumerate(roman):
-                        start = m.start()
-                        end = roman[i + 1].start() if i + 1 < len(roman) else len(content)
-                        chunks.append({
-                            "q_no": f'{current["q_no"]}.{m.group(1).lower()}',
-                            "content": content[start:end].strip()
-                        })
-                else:
-                    chunks.append(current)
-            q = re.search(r'Q\.?\s*(\d+)|^(\d+)\.', line, re.I)
-            num = q.group(1) if q.group(1) else q.group(2)
-            current = {
-                "q_no": f"Q{num}",
-                "content": line
-            }
-        elif current:
-            current["content"] += " " + line
-    if current:
-        chunks.append(current)
-    return chunks
